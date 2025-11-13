ca4897421136a143fb4388ba021af93a238d64a6
dsv999 <djalil.suleymanov@gmail.com>
2025-11-13 12:17:23 +0300

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,13 +1,13 @@
 const listItems2 = document.querySelectorAll("img")
 
+let classes = []
+
 function toggleDone(e) {
- if (!e.target.className){
-  e.target.className = "flip_vert"
- } else {
-  e.target.className = ""
- }
+  if (e.button === 0) e.target.classList.toggle(`flip_vert`)
+  else if (e.button === 2) e.target.classList.toggle(`flip_gor`)
 }
 
 listItems2.forEach((item) => {
-  item.addEventListener("click", toggleDone);
-})
+  item.addEventListener("mousedown", toggleDone);
+  item.addEventListener("contextmenu", (e) => e.preventDefault());
+});
